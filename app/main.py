84ca11c247d09e98d7ba77a4e856c0468a1fe4cd
Abir Deb <adeb09@gmail.com>
2026-02-27 22:30:57 -0500

    echos last message from the chat completions v1 openAI style API

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+from os import lstat
+
+from fastapi import FastAPI, Header
+import uuid
+from random import randint
 
 from models import ChatCompletionRequest
 
@@ -13,8 +17,26 @@ def root():
 
 
 @app.post("/v1/chat/completions")
-async def completions(request: ChatCompletionRequest):
-    return request
+async def completions(request: ChatCompletionRequest,
+                      x_request_id: str = Header(None)):
+    if not x_request_id:
+        x_request_id = str(uuid.uuid4())
+
+    # echo last message in the chat
+    for msg in reversed(request.messages):
+        role = msg.role
+        last_msg = msg.content
+
+    # mocking calculation for token usage for now
+    prompt_tokens = len(last_msg)
+    completion_tokens = len(last_msg) # we're just echoing the last message back for now
+    total_tokens = prompt_tokens + completion_tokens
+
+    return {
+        "request-id": x_request_id,
+        "choices": [{"messages": {"role": role, "content": last_msg}, "finish_reason": "stop"}],
+        "usage": {"prompt_tokens": prompt_tokens, "completion_tokens": completion_tokens, "total_tokens": total_tokens}
+    }
 
 
 if __name__ == "__main__":
